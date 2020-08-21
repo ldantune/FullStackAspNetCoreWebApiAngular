f46f53f4f88b84270a5812b8a97290ebb129d0e6
Lucas Antunes <lucasdanielantunes@gmail.com>
2020-08-20 22:41:55 -0300

    Atualização do projeto

--- ProAgil.API/Controllers/ValuesController.cs
+++ ProAgil.API/Controllers/ValuesController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using ProAgil.API.Model;
 
 namespace ProAgil.API.Controllers
 {
@@ -12,9 +13,18 @@ namespace ProAgil.API.Controllers
     {
         // GET api/values
         [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
+        public ActionResult<IEnumerable<Evento>> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new Evento[] { 
+                new Evento() {
+                    EventoId = 1,
+                    Tema = "Angular",
+                    Local = "Uberaba",
+                    Lote = "1° Lote",
+                    QtdPessoas = 250,
+                    DataEvento = DateTime.Now.AddDays(2).ToString("dd/MM/yyyy")
+                }
+             };
         }
 
         // GET api/values/5
